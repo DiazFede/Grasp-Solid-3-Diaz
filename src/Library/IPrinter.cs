1718457e9ff5aa2cf550c741a0b58ed70e54a0df
DiazFede <diazfedericoucu@gmail.com>
2023-09-24 20:33:13 -0300

    Se agrego comentario en la interfaz

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -8,3 +8,10 @@ namespace Full_GRASP_And_SOLID.Library
        public void PrintRecipe(Recipe recipe);       
     }
 }
+
+
+
+//Para asignar la responsabilidad de impresion en diferentes destinos se esta utilizando el single responsibility principle (srp),
+//para que cada clase tenga la unica responsabilidad de imprimir en un lugar especifico. Ademas, se puede decir que los objetos son
+//tratados de manera polimorfica, ya que se puede llamar al metodo printrecipe definido en la interfaz. 
+//Por ultimo, se cumple el LSP, debido a que el comportamiento de printrecipe es compatible en todas las implemetaciones.
